d9c5cef18a743ead436445d7086f0e2c5094b4b7
Matt Mower <mowerm@gmail.com>
2013-01-18 01:52:17 -0600

    ramdisk and proprietaries

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,15 +31,14 @@
 TARGET_BOOTLOADER_BOARD_NAME := fireball
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 # androidboot.hardware=fighter
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=fighter
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 BOARD_KERNEL_PAGESIZE := 2048
 
 # TARGET_PREBUILT_KERNEL := device/htc/fireball/kernel
 TARGET_KERNEL_CONFIG := fighter_defconfig
-#TARGET_KERNEL_SOURCE := kernel/htc/msm8960
-TARGET_KERNEL_SOURCE := kernel/htc/iv-msm8960
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 
 # Lights
 TARGET_PROVIDES_LIBLIGHTS := true
