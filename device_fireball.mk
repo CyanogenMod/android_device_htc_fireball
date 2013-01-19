a8cdf33a7a872e6218b268733c3eebe6ef462aab
jared waters <kushdeck@gmail.com>
2013-01-18 22:42:35 -0800

    fireball: inherit dalvik heap config.

--- device_fireball.mk
+++ device_fireball.mk
@@ -111,6 +111,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/htc/fireball/fireball-vendor.mk)
 
+# call dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
 # Discard inherited values and use our own instead.
 PRODUCT_DEVICE := fireball
 PRODUCT_NAME := fireball
