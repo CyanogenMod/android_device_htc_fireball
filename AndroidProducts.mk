de4a60e6f94ec7f886ddd9a7074ce649e84915bb
Matt Mower <mowerm@gmail.com>
2013-05-13 06:05:50 -0500

    Licensing: copyright and licensing in all teh files
    Change-Id: Ib53270f21628063be888962b2f7b7e65fb471f6e

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
-#
+# Copyright (C) 2013 The CyanogenMod Project
 # Copyright (C) 2011 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/device_fireball.mk
