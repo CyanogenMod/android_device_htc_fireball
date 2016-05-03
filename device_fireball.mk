5b0627db921ad41c5eacd32f5565d257ba2344b3
Matt Mower <mowerm@gmail.com>
2016-05-03 01:24:57 -0500

    fireball: Remove unused sound config
    Change-Id: Ic5d0514bd982f0a21aad7e24ad5f90465b0104ef

--- device_fireball.mk
+++ device_fireball.mk
@@ -24,16 +24,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc
 
-# Sound configs
-PRODUCT_COPY_FILES += \
-    device/htc/fireball/dsp/soundimage/srs_bypass.cfg:system/etc/soundimage/srs_bypass.cfg \
-    device/htc/fireball/dsp/soundimage/srsfx_trumedia_51.cfg:system/etc/soundimage/srsfx_trumedia_51.cfg \
-    device/htc/fireball/dsp/soundimage/srsfx_trumedia_movie.cfg:system/etc/soundimage/srsfx_trumedia_movie.cfg \
-    device/htc/fireball/dsp/soundimage/srsfx_trumedia_music.cfg:system/etc/soundimage/srsfx_trumedia_music.cfg \
-    device/htc/fireball/dsp/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
-    device/htc/fireball/dsp/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
-    device/htc/fireball/dsp/soundimage/srs_global.cfg:system/etc/soundimage/srs_global.cfg
-
+# Sound config
 PRODUCT_COPY_FILES += \
     device/htc/fireball/dsp/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
 
