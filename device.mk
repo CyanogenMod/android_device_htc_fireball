666426b23b76816f12da17eb3ebea2f06deb8c47
Matt Mower <mowerm@gmail.com>
2016-06-10 15:22:51 -0500

    fireball: Remove redundant languages inheritance
    specified by device: full_base_telephony.mk
    inherits: aosp_base.mk
    inherits: full_base.mk
    inherits: locales_full.mk
    inherits: languages_full.mk
    
    Change-Id: Ia862616a59868ba4d9bd9359f7750fe9aff47566

--- device.mk
+++ device.mk
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/htc/fireball/fireball-vendor.mk)
 
