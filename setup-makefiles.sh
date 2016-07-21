3d5d148f21e764d83e18cb404bcd7eb3370e37b6
Matt Mower <mowerm@gmail.com>
2016-07-21 15:27:47 -0500

    fireball: Use common extract-utils
    Change-Id: I6a356ebd92390ff78fc107337af394cb4b2b901e

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,5 +17,7 @@
 set -e
 
 export DEVICE=fireball
+export DEVICE_COMMON=s4-common
 export VENDOR=htc
-./../s4-common/setup-makefiles.sh $@
+
+./../$DEVICE_COMMON/setup-makefiles.sh $@
